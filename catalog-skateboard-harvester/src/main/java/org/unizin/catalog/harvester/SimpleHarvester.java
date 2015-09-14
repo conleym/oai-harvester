7baba13a7d9281ec70205ab2b66a3f6dcbfb03f7
Steven Huwig <steve.huwig@unizin.org>
2015-09-14 11:05:12 -0400

    Most rudimentary harvest possible

--- catalog-skateboard-harvester/src/main/java/org/unizin/catalog/harvester/SimpleHarvester.java
+++ catalog-skateboard-harvester/src/main/java/org/unizin/catalog/harvester/SimpleHarvester.java
@@ -1,16 +1,27 @@
 package org.unizin.catalog.harvester;
 
+import org.apache.commons.io.output.NullOutputStream;
 import org.unizin.catalog.harvester.config.ConfigReader;
 import org.unizin.catalog.harvester.config.Repository;
+import org.unizin.catalog.harvester.oai.OAIClient;
 
 import javax.xml.bind.JAXBException;
+import javax.xml.stream.XMLStreamException;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class SimpleHarvester {
-    public static void main(String[] args) throws JAXBException {
+    public static void main(String[] args) throws
+            JAXBException,
+            IOException,
+            XMLStreamException {
         ConfigReader reader = new ConfigReader();
+        OAIClient client = new OAIClient();
         for (Repository repository : reader.parse(new File("repositories.xml"))) {
-            System.out.println(repository.url);
+            try (FileOutputStream out = new FileOutputStream(repository.name + ".xml")) {
+                client.listRecords(repository.url, out);
+            }
         }
     }
 
