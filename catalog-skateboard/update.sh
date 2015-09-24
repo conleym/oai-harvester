f92340ac47f65d010024598a09b9c2295bf1d4ab
Asa Ayers <Asa@AsaAyers.com>
2015-09-24 09:33:09 -0400

    Change update.sh to require the VM's name as a parameter

--- catalog-skateboard/update.sh
+++ catalog-skateboard/update.sh
@@ -2,14 +2,26 @@
 
 set -ex
 
+if [ -z "$1" ]; then
+    echo "USAGE: $0 VMName"
+    echo "VMName should be whatever you use to ssh to your nuxeo VM"
+    exit 1
+fi
+
+VM_NAME=$1
+
+# verify the name works before rebuilding everything
+ssh $VM_NAME "true" || exit 1
+
+
 mvn clean package
 
 SRC=$(ls catalog-skateboard-marketplace-package/target/*.zip)
 FILE=$(basename $SRC)
 
-scp $SRC nuxeo:/tmp/$FILE
+scp $SRC $VM_NAME:/tmp/$FILE
 
-ssh -q nuxeo << EOF
+ssh -q $VM_NAME << EOF
 sudo service nuxeo stop
 sudo nuxeoctl mp-remove catalog-skateboard
 sudo nuxeoctl mp-install /tmp/$FILE
