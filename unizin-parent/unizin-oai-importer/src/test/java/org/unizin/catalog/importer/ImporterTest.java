6dfe0b321139c72f17552a256ce2b2ccca0178ce
Mike Conley <conasdf@gmail.com>
2015-09-28 16:52:36 -0400

    Remove redundant features from the test class.
    If we use ">" to mean "includes",
      PlatformFeature > CoreFeature > RuntimeFeature
    
    We need PlatformFeature, so the other two are redundant.

--- unizin-parent/unizin-oai-importer/src/test/java/org/unizin/catalog/importer/ImporterTest.java
+++ unizin-parent/unizin-oai-importer/src/test/java/org/unizin/catalog/importer/ImporterTest.java
@@ -10,7 +10,6 @@ import org.junit.runner.RunWith;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.DocumentModelList;
-import org.nuxeo.ecm.core.test.CoreFeature;
 import org.nuxeo.ecm.core.test.TransactionalFeature;
 import org.nuxeo.ecm.platform.importer.base.GenericMultiThreadedImporter;
 import org.nuxeo.ecm.platform.importer.base.ImporterRunnerConfiguration.Builder;
@@ -21,13 +20,11 @@ import org.nuxeo.ecm.platform.test.PlatformFeature;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
-import org.nuxeo.runtime.test.runner.RuntimeFeature;
 
 import com.google.inject.Inject;
 
 @RunWith(FeaturesRunner.class)
-@Features({TransactionalFeature.class, RuntimeFeature.class, CoreFeature.class,
-	PlatformFeature.class})
+@Features({TransactionalFeature.class, PlatformFeature.class})
 @Deploy({"org.unizin.cmp.schemas"})
 public final class ImporterTest {
 	private static final String ZIP_NAME = "/harvester-test.zip";
