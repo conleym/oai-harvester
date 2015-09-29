fb838c78b651647db9f69aec46bf928d88cb6202
Erik Goens <egoens@gmail.com>
2015-09-29 12:47:35 -0600

    adds mixins for global styles improves ui for detail view

--- unizin-parent/unizin-search/src/main/js/containers/result.jsx
+++ unizin-parent/unizin-search/src/main/js/containers/result.jsx
@@ -5,6 +5,7 @@ import { ensureDocument } from '../actions/documents.js'
 import { routeInsert, routePreviewUrl } from '../actions/route.js'
 import styles from './result.scss'
 import { Link } from 'react-router'
+import classNames from 'classnames'
 
 class Result extends React.Component {
     static displayName = 'Result'
@@ -20,45 +21,60 @@ class Result extends React.Component {
         }
 
         const previewUrl = routePreviewUrl(document).url
+        const primaryBtnClasses = classNames("btn", "primary")
 
         return (
             <main className={styles.result} role="main">
-              <navigation>
-                <button onClick={this.props.history.goBack} aria-role="button">
+              <div className={styles.header}>
+                <button onClick={this.props.history.goBack} role="button">
                     &lt; Back to results
                 </button>
 
                 <ul className={styles.controls}>
                   <li>
+                    <Link to={routeInsert(document).route} className={primaryBtnClasses} role="button">
+                      + Insert
+                    </Link>
+                  </li>
+                  <li>
                     <a href={previewUrl} target="_blank" className={styles.btn}>
                       o Preview
                     </a>
                   </li>
-                  <li>
-                    <Link to={routeInsert(document).route} className={styles.btn}>
-                      + Insert
-                    </Link>
-                  </li>
                 </ul>
-              </navigation>
+              </div>
 
               <div className={styles.wrapper}>
                 <div className={styles.content}>
 
                   <Cover document={document} className={styles.cover} />
 
-                  <h1>{document.title}</h1>
+                  <div className={styles.details}>
 
-                  {JSON.stringify(document, null, 2)}
+                    <h1>{document.title}</h1>
+
+                    <h2>Author: {document.properties['hrv:creator']}</h2>
+
+                    <div className={styles.description}>
+                      {document.properties['hrv:description']}
+                    </div>
+                  </div>
+
                 </div>
 
-                <aside>
+                <aside role="complementary">
                   <h1>Additional information</h1>
                   <ul>
-                    <li>Some info</li>
-                    <li>More info</li>
+                    <li><span>Format</span>{document.type}</li>
+                    <li><span>File size</span>{(document.properties["common:size"])}</li>
+                    <li><span>Language</span>{document.properties["hrv:language"]}</li>
+                    <li><span>Date added</span>{document.properties["hrv:date"]}</li>
+                    <li><span>Rights</span>{document.properties["hrv:rights"]}</li>
                   </ul>
                 </aside>
+              </div>
+              <div className={styles.footer}>
+                powered by [Unizin Logo]
               </div>
             </main>
         )
