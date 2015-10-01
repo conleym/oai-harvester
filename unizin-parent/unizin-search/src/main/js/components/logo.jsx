3ad57426f1e33e69648a8a0c12bb46438facfc5c
Asa Ayers <Asa@AsaAyers.com>
2015-10-01 13:58:52 -0400

    Forgot className

--- unizin-parent/unizin-search/src/main/js/components/logo.jsx
+++ unizin-parent/unizin-search/src/main/js/components/logo.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 export default class Logo extends React.Component {
 
+    static propTypes = {
+        className: React.PropTypes.string
+    }
+
     render() {
         const logoURL = require('file!../../resources/skin/resources/logo.svg')
         return (
-            <img src={logoURL} />
+            <img className={this.props.className} src={logoURL} />
         )
     }
 }
