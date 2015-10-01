7eb71e88b80cc9d888a3475e79ad7d90fad023be
Erik Goens <egoens@gmail.com>
2015-10-01 12:06:59 -0600

    removes unused constructor

--- unizin-parent/unizin-search/src/main/js/components/footer.jsx
+++ unizin-parent/unizin-search/src/main/js/components/footer.jsx
@@ -5,10 +5,6 @@ import React, { PropTypes } from 'react'
 
 export default class Footer extends React.Component {
 
-    constructor(props, context) {
-        super(props, context)
-    }
-
     render() {
         return (
           <div className={styles.footer}>
