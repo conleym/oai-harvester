c2e7ec3e33b34e35309f505d258a45b62ba1d949
Asa Ayers <Asa@AsaAyers.com>
2015-10-02 10:29:26 -0400

    classNames

--- unizin-parent/unizin-search/src/main/js/components/loading.jsx
+++ unizin-parent/unizin-search/src/main/js/components/loading.jsx
@@ -12,9 +12,7 @@ export default class Loader extends React.Component {
     render() {
         const logoURL = require('file!../../resources/skin/resources/logo.svg')
 
-        const loadingClasses = classNames(styles.loading, {
-            [this.props.className]: (this.props.className)
-        })
+        const loadingClasses = classNames(styles.loading, this.props.className)
 
         return (
             <div className={loadingClasses}>
