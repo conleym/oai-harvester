2e76b731280db8688825044d740deb4d9347f6ee
Erik Goens <egoens@gmail.com>
2015-10-02 09:31:45 -0600

    changes branding on loading animation forces loading view for testing purposes

--- unizin-parent/unizin-search/src/main/js/components/search_results.jsx
+++ unizin-parent/unizin-search/src/main/js/components/search_results.jsx
@@ -81,9 +81,11 @@ export default class SearchResults extends React.Component {
     render() {
         const { criteria, results } = this.props
 
-        if (results.totalSize == null) {
-            return <Loading message={`Loading Results for '${criteria.text}'`} className={styles.loading} />
-        }
+        return <Loading message={`Loading Results for '${criteria.text}'`} className={styles.loading} />
+
+        // if (results.totalSize == null) {
+        //     return <Loading message={`Loading Results for '${criteria.text}'`} className={styles.loading} />
+        // }
 
         const { totalSize = 0, pageSize = 20 } = results
 
