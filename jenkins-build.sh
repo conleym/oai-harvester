a731eb0cd69fda81a21a4b7f498219df08a91aa4
Mike Conley <conasdf@gmail.com>
2015-10-13 11:31:01 -0400

    Add build script.
    Move the build commands out of jenkins and into a separate script so that it
    can be reused (e.g., for pull request builds).
    
    Rename the upload script so that it's obvious that it runs on jenkins and so
    that tab completion of update.sh goes smoothly.

---
+++ jenkins-build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Usage: jenkins-build.sh MAVEN_TARGETS
+
+export JAVA_HOME="/var/lib/jenkins/tools/hudson.model.JDK/Java_1.8"
+export M2_HOME="/var/lib/jenkins/tools/hudson.tasks.Maven_MavenInstallation/nuxeo"
+"${M2_HOME}/bin/mvn" -f unizin-parent/ "$@"
+"${M2_HOME}/bin/mvn" -f unizin-harvester/ "$@"
