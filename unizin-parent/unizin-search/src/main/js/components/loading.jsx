ca7321894b7f71a2f58f41ce4c4d4a9cdfd7a112
Erik Goens <egoens@gmail.com>
2015-10-16 12:57:42 -0600

    provides more verbose messaging for insert iterim view

--- unizin-parent/unizin-search/src/main/js/components/loading.jsx
+++ unizin-parent/unizin-search/src/main/js/components/loading.jsx
@@ -6,7 +6,8 @@ export default class Loader extends React.Component {
     static displayName = 'Loader'
 
     static propTypes = {
-        message: React.PropTypes.string
+        message: React.PropTypes.string,
+        className: React.PropTypes.string
     }
 
     static defaultProps = {
