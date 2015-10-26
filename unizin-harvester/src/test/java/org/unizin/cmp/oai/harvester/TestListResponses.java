95f0cb3a4ce5227c440fda16b8a52a72bef544c3
Mike Conley <conasdf@gmail.com>
2015-10-25 20:00:37 -0400

    Add test for badResumptionToken errors.

--- unizin-harvester/src/test/java/org/unizin/cmp/oai/harvester/TestListResponses.java
+++ unizin-harvester/src/test/java/org/unizin/cmp/oai/harvester/TestListResponses.java
@@ -13,6 +13,7 @@ import java.util.Observer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -20,15 +21,20 @@ import org.mockito.AdditionalMatchers;
 import org.mockito.Mockito;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.unizin.cmp.oai.OAIError;
+import org.unizin.cmp.oai.OAIErrorCode;
 import org.unizin.cmp.oai.OAIVerb;
 import org.unizin.cmp.oai.ResumptionToken;
+import org.unizin.cmp.oai.harvester.exception.OAIProtocolException;
 import org.unizin.cmp.oai.harvester.response.OAIResponseHandler;
 import org.unizin.cmp.oai.mocks.Mocks;
 import org.unizin.cmp.oai.mocks.NotificationMatchers;
+import org.unizin.cmp.oai.templates.ErrorsTemplate;
 import org.unizin.cmp.oai.templates.ListRecordsTemplate;
 import org.unizin.cmp.oai.templates.RecordMetadataTemplate;
 
 import freemarker.template.TemplateException;
+
 
 public final class TestListResponses extends HarvesterTestBase {
 	private static final Logger LOGGER = 
@@ -38,9 +44,25 @@ public final class TestListResponses extends HarvesterTestBase {
 	public ExpectedException exception = ExpectedException.none();
 
 	private final ResumptionToken firstToken = 
-			new ResumptionToken("the response token", 2L, 1L, null);
+			new ResumptionToken("the first token", 2L, 1L, null);
 	private final ResumptionToken lastToken = 
 			new ResumptionToken("", 2L, 2L, null);
+
+	private static Map<String, Object> toMap(final ResumptionToken token) {
+		final Map<String, Object> m = new HashMap<>();
+		m.put("token", token.getToken());
+		if (token.getCursor().isPresent()) {
+			m.put("cursor", token.getCursor().get());
+		}
+		if (token.getCompleteListSize().isPresent()) {
+			m.put("completeListSize", 
+					token.getCompleteListSize().get());
+		}
+		if (token.getExpirationDate().isPresent()) {
+			m.put("expirationDate", token.getExpirationDate().get());
+		}
+		return m;
+	}
 
 	/**
 	 * Set up with two incomplete lists. First has two records, second has one.
@@ -53,15 +75,12 @@ public final class TestListResponses extends HarvesterTestBase {
 	private void setupWithDefaultListRecordsResponse(
 			final boolean sendFinalResumptionToken) 
 					throws TemplateException, IOException {
-		final Map<String, Object> token = new HashMap<>();
 		final Map<String, Object> record = new HashMap<>();
 
-		token.put("token", firstToken.getToken());
-		token.put("cursor", firstToken.getCursor().get());
-		token.put("completeListSize", firstToken.getCompleteListSize().get());
 		ListRecordsTemplate listRecordsTemplate = new ListRecordsTemplate()
-				.withResumptionToken(token);
-		RecordMetadataTemplate recordMetadataTemplate = new RecordMetadataTemplate()
+				.withResumptionToken(toMap(firstToken));
+		RecordMetadataTemplate recordMetadataTemplate = 
+				new RecordMetadataTemplate()
 				.addTitle("A Title")
 				.addCreator("Some Creator")
 				.addCreator("Another Creator");
@@ -81,12 +100,7 @@ public final class TestListResponses extends HarvesterTestBase {
 
 		listRecordsTemplate = new ListRecordsTemplate();
 		if (sendFinalResumptionToken) { 
-			token.clear();
-			token.put("token", lastToken.getToken());
-			token.put("cursor", lastToken.getCursor().get());
-			token.put("completeListSize", 
-					lastToken.getCompleteListSize().get());
-			listRecordsTemplate.withResumptionToken(token);
+			listRecordsTemplate.withResumptionToken(toMap(lastToken));
 		}
 		recordMetadataTemplate = new RecordMetadataTemplate()
 				.addTitle("Such Title Wow");
@@ -166,15 +180,54 @@ public final class TestListResponses extends HarvesterTestBase {
 		inOrderVerify(obs).update(eq(harvester), hrvEnded.get());
 	}
 
+	/**
+	 * Tests that the harvester correctly stops when an empty
+	 * &lt;resumptionToken&gt; element is found in the response.
+	 */
 	@Test
 	public void testListRecords() throws Exception {
 		setupWithDefaultListRecordsResponse(true);
 		listRecordsTest(2, Arrays.asList(firstToken, lastToken));
 	}
 
+	/**
+	 * Tests that the harvester correctly stops when no &lt;resumptionToken&gt;
+	 * element is found in the response.
+	 */
 	@Test
 	public void testListRecordsWithNoFinalResumptionToken() throws Exception {
 		setupWithDefaultListRecordsResponse(false);
 		listRecordsTest(2, Arrays.asList(firstToken, new ResumptionToken("")));
+	}
+
+	/**
+	 * Tests that the harvester handles the
+	 * {@link OAIErrorCode#BAD_RESUMPTION_TOKEN} error correctly.
+	 */
+	@Test
+	public void testListRecordsWithBadResumptionToken() throws Exception {
+		final ListRecordsTemplate lrt = new ListRecordsTemplate()
+				.withResumptionToken(toMap(firstToken));
+		final Map<String, Object> record = new HashMap<>();
+		record.put("metadata", new RecordMetadataTemplate().process());
+		lrt.addRecord(record);
+		final String firstResp = lrt.process();
+		LOGGER.debug("First response: {}", firstResp);
+		mockHttpClient.addResponseFrom(200, "", firstResp);
+		final List<OAIError> errors = Arrays.asList(new OAIError(
+				OAIErrorCode.BAD_RESUMPTION_TOKEN.code(),
+				"A helpful message."));
+		final String secondResp = ErrorsTemplate.process(errors);
+		LOGGER.debug("Second response: {}", secondResp);
+		mockHttpClient.addResponseFrom(200, "", secondResp);
+		final Harvester harvester = defaultTestHarvester();
+		final OAIResponseHandler h = Mocks.newResponseHandler();
+		exception.expect(OAIProtocolException.class);
+		try {
+			harvester.start(defaultTestParams(OAIVerb.LIST_RECORDS), h);
+		} catch (final OAIProtocolException e) {
+			Assert.assertEquals(errors, e.getOAIErrors());
+			throw e;
+		}
 	}
 }
