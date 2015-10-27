cab2b0f26471dbb6b49ea5fe4f6303e49e3cc127
Erik Goens <egoens@gmail.com>
2015-10-27 14:46:59 -0600

    adds styles for contribution form progress adds variable for success color

--- unizin-parent/unizin-ingest/src/main/js/components/file_upload.jsx
+++ unizin-parent/unizin-ingest/src/main/js/components/file_upload.jsx
@@ -19,20 +19,27 @@ export default class FileUpload extends React.Component {
     render() {
         const { name, size, thumbnail, progress, error } = this.props
         const filesize = fileSize(size)
+        const progressWidth = `${progress}%`
+        const progressLabel = (progress === 100) ? "Complete" : "Uploading"
 
         return (
             <div className={styles.preview}>
                 <h2 className="aural">File details</h2>
                 <div className={styles.details}>
-                    <div className={styles.thumbnail}>
-                      <img src={thumbnail} alt={thumbnail} title="File thumbnail" aria-hidden="true" />
+                    <div className={styles.thumbnail} aria-hidden="true">
+                      <img src={thumbnail} alt={thumbnail} title="File thumbnail" />
                     </div>
                     <div>
                       {name}<span className={styles.size}>({filesize})</span>
                     </div>
                 </div>
-                <div className={styles.progress}>
-                    {progress}
+                <div className={styles.progressWrapper}>
+                    <div className={styles.progressLabel}>
+                      {progress} {progressLabel}
+                    </div>
+                    <div className={styles.barWrapper}>
+                      <div className={styles.bar} style={{width: progressWidth}}></div>
+                    </div>
                 </div>
                 <div className={styles.error}>
                     {error}
