c6cf6e98416e670f6a48e6ee9aada0ecdb5eeeaa
Steven Huwig <steve.huwig@unizin.org>
2015-10-27 11:21:49 -0400

    remove unneeded changes

--- unizin-parent/unizin-retrieval/src/test/java/org/unizin/cmp/retrieval/CopyFromSourceRepositoryTest.java
+++ unizin-parent/unizin-retrieval/src/test/java/org/unizin/cmp/retrieval/CopyFromSourceRepositoryTest.java
@@ -25,7 +25,6 @@ import org.nuxeo.ecm.core.test.annotations.Granularity;
 import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
 import org.nuxeo.ecm.core.work.api.WorkManager;
 import org.nuxeo.ecm.platform.test.PlatformFeature;
-import org.nuxeo.ecm.platform.usermanager.UserManager;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
@@ -63,9 +62,6 @@ public class CopyFromSourceRepositoryTest {
 
     @Inject
     WorkManager workManager;
-
-    @Inject
-    UserManager userManager;
 
     private DocumentModel inputDoc;
 
