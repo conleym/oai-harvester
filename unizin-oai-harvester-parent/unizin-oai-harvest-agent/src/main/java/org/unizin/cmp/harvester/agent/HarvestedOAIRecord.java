0d80747fa7501ba893e5f53357c50e486795b798
Mike Conley <conasdf@gmail.com>
2015-11-02 18:19:51 -0500

    Add class for DynamoDB mapper to shit into the database.

---
+++ unizin-oai-harvester-parent/unizin-oai-harvest-agent/src/main/java/org/unizin/cmp/harvester/agent/HarvestedOAIRecord.java
@@ -0,0 +1,177 @@
+package org.unizin.cmp.harvester.agent;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBIndexRangeKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+/**
+ * DynamoDB mapped harvested record.
+ * <p>
+ * Getters and setters are required by the DynamoDB wrapper.
+ *
+ */
+@DynamoDBTable(tableName = HarvestedOAIRecord.TABLE_NAME)
+public final class HarvestedOAIRecord {
+    public static final String TABLE_NAME = "HarvestedOAIRecords";
+    public static final String OAI_ID_ATTRIB = "oai-identifier";
+    public static final String DATESTAMP_ATTRIB = "datestamp";
+    public static final String SETS_ATTRIB = "sets";
+    public static final String BASE_URL_ATTRIB = "base-url";
+    public static final String XML_ATTRIB = "xml";
+    public static final String CHECKSUM_ATTRIB = "xml-checksum";
+    public static final String STATUS_ATTRIB = "status";
+
+
+    @DynamoDBAttribute(attributeName = BASE_URL_ATTRIB)
+    @DynamoDBHashKey
+    private String baseURL;
+
+    @DynamoDBAttribute(attributeName = OAI_ID_ATTRIB)
+    @DynamoDBRangeKey
+    private String identifier;
+
+    @DynamoDBAttribute(attributeName = SETS_ATTRIB)
+    private Set<String> sets = new HashSet<>();
+
+    @DynamoDBIndexRangeKey(localSecondaryIndexName = "DatestampIndex")
+    @DynamoDBAttribute(attributeName = DATESTAMP_ATTRIB)
+    private String datestamp;
+
+    @DynamoDBAttribute(attributeName = XML_ATTRIB)
+    private String xml;
+
+    @DynamoDBAttribute(attributeName = CHECKSUM_ATTRIB)
+    private byte[] checksum;
+
+    @DynamoDBAttribute(attributeName = STATUS_ATTRIB)
+    private String status;
+
+    public String getBaseURL() {
+        return baseURL;
+    }
+
+    public void setBaseURL(final String baseURL) {
+        this.baseURL = baseURL;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(final String identifier) {
+        this.identifier = identifier;
+    }
+
+    public Set<String> getSets() {
+        return sets;
+    }
+
+    public void setSets(final Set<String> sets) {
+        this.sets = sets;
+    }
+
+    public String getDatestamp() {
+        return datestamp;
+    }
+
+    public void setDatestamp(final String datestamp) {
+        this.datestamp = datestamp;
+    }
+
+    public String getXml() {
+        return xml;
+    }
+
+    public void setXml(final String xml) {
+        this.xml = xml;
+    }
+
+    public byte[] getChecksum() {
+        return checksum;
+    }
+
+    public void setChecksum(final byte[] checksum) {
+        this.checksum = checksum;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(final String status) {
+        this.status = status;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((baseURL == null) ? 0 : baseURL.hashCode());
+        result = prime * result + Arrays.hashCode(checksum);
+        result = prime * result + ((datestamp == null) ? 0 : datestamp.hashCode());
+        result = prime * result + ((identifier == null) ? 0 : identifier.hashCode());
+        result = prime * result + ((sets == null) ? 0 : sets.hashCode());
+        result = prime * result + ((status == null) ? 0 : status.hashCode());
+        result = prime * result + ((xml == null) ? 0 : xml.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        HarvestedOAIRecord other = (HarvestedOAIRecord) obj;
+        if (baseURL == null) {
+            if (other.baseURL != null)
+                return false;
+        } else if (!baseURL.equals(other.baseURL))
+            return false;
+        if (!Arrays.equals(checksum, other.checksum))
+            return false;
+        if (datestamp == null) {
+            if (other.datestamp != null)
+                return false;
+        } else if (!datestamp.equals(other.datestamp))
+            return false;
+        if (identifier == null) {
+            if (other.identifier != null)
+                return false;
+        } else if (!identifier.equals(other.identifier))
+            return false;
+        if (sets == null) {
+            if (other.sets != null)
+                return false;
+        } else if (!sets.equals(other.sets))
+            return false;
+        if (status == null) {
+            if (other.status != null)
+                return false;
+        } else if (!status.equals(other.status))
+            return false;
+        if (xml == null) {
+            if (other.xml != null)
+                return false;
+        } else if (!xml.equals(other.xml))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getName() + " [baseURL=" + baseURL +
+                ", identifier=" + identifier + ", sets=" + sets +
+                ", datestamp=" + datestamp + ", xml=" + xml +
+                ", checksum=" + Arrays.toString(checksum) + ", status="+ status
+                + "]";
+    }
+}
