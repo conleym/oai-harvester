5cee248c0defb4575d93092dc696c289fbb7f2db
Mike Conley <conasdf@gmail.com>
2015-11-16 22:08:56 -0500

    Refactor running harvester management into a separate class.
    Also stop harvests if we time out when offering records to the queue, and make
    clear that each HarvestAgent is single-use. Probably we'll end up making one per
    harvest, and we'll definitely not reuse them.
    
    TODO: should we _really_ be shutting the executor down when stopping? I want
    to stop all the threads, but I might also want to reuse this pool....

---
+++ unizin-oai-harvester-parent/unizin-oai-harvest-agent/src/main/java/org/unizin/cmp/harvester/agent/RunningHarvesters.java
@@ -0,0 +1,77 @@
+package org.unizin.cmp.harvester.agent;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.unizin.cmp.oai.harvester.Harvester;
+
+final class RunningHarvesters {
+    private final Object lock = new Object();
+    private final Set<Harvester> harvesters = new HashSet<>();
+
+    private void add(final Harvester harvester) {
+        synchronized(lock) {
+            harvesters.add(harvester);
+        }
+    }
+
+    boolean remove(final Harvester harvester) {
+        synchronized(lock) {
+            return harvesters.remove(harvester);
+        }
+    }
+
+    /**
+     * Are there any running harvests?
+     *
+     * @return {@code true} iff there are no running harvests.
+     */
+    boolean isEmpty() {
+        synchronized(lock) {
+            return harvesters.isEmpty();
+        }
+    }
+
+    /** Clear the running harvests. */
+    void clear() {
+        synchronized(lock) {
+            harvesters.clear();
+        }
+    }
+
+    /**
+     * Stop all running harvests.
+     */
+    void stopAll() {
+        synchronized(lock) {
+            harvesters.forEach(Harvester::stop);
+        }
+    }
+
+    /**
+     * Create and return a {@code Runnable} for a managed harvest.
+     * <p>
+     * This method first adds the given key, harvester pair to itself, then
+     * produces a new {@code Runnable} that executes the given {@code Runnable}.
+     * When its execution completes, the key-harvester pair will be removed.
+     *
+     * @param key
+     *            the key identifying the harvest.
+     * @param harvester
+     *            the harvester itself.
+     * @param runnable
+     *            arbitrary code to run. The most logical use is to run a
+     *            harvest, but clients can do anything.
+     * @return a {@code Runnable} as described above.
+     */
+    Runnable wrappedRunnable(final Harvester harvester, final Runnable runnable) {
+        add(harvester);
+        return () -> {
+            try {
+                runnable.run();
+            } finally {
+                remove(harvester);
+            }
+        };
+    }
+}
