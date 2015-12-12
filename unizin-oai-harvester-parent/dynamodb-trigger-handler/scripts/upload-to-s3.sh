d59e894aadeeef3d9b387bee56d71cfe45b6e4b1
Mike Conley <conasdf@gmail.com>
2015-12-12 13:12:17 -0500

    Remove anachronisms from S3 upload script.

--- unizin-oai-harvester-parent/dynamodb-trigger-handler/scripts/upload-to-s3.sh
+++ unizin-oai-harvester-parent/dynamodb-trigger-handler/scripts/upload-to-s3.sh
@@ -4,12 +4,11 @@ set -e
 
 BUCKET="unizin-lambda-functions"
 
-# The name of the maven project (and thus the resulting jar) is the name of the
-# current directory. We need to grab this because the build produces more than
-# one jar, and we want to upload only one.
+# The name of the maven project (and thus the resulting zip file) is the name of
+# the current directory.
 PWD=$(pwd)
 PROJ=$(basename "${PWD}")
-JAR=$(find target -name "${PROJ}-*.zip")
-JAR="${PWD}/${JAR}"
-echo "Uploading ${JAR} to ${BUCKET}."
-aws s3 cp "${JAR}" "s3://${BUCKET}/"
+ZIP=$(find target -name "${PROJ}-*.zip")
+ZIP="${PWD}/${ZIP}"
+echo "Uploading ${ZIP} to ${BUCKET}."
+aws s3 cp "${ZIP}" "s3://${BUCKET}/"
