f1b40dd35878e3b0bc56b037c750afcaf3a4e0ea
Mike Conley <conasdf@gmail.com>
2015-12-26 13:32:36 -0500

    BufferAction contains the action to take.
    I like this _much_ better. Way less error-prone and easier to modify.

--- unizin-oai-harvester-parent/unizin-oai-harvester/src/main/java/org/unizin/cmp/oai/harvester/response/BufferingOAIEventHandler.java
+++ unizin-oai-harvester-parent/unizin-oai-harvester/src/main/java/org/unizin/cmp/oai/harvester/response/BufferingOAIEventHandler.java
@@ -2,6 +2,7 @@ package org.unizin.cmp.oai.harvester.response;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
@@ -11,31 +12,43 @@ import org.unizin.cmp.oai.harvester.Functions;
 
 
 public class BufferingOAIEventHandler implements OAIEventHandler {
-    public static enum BufferAction {
-        /** Discard the buffer contents. Do not the current event. */
-        DISCARD,
+    public static enum BufferAction
+    implements BiConsumer<BufferingOAIEventHandler, XMLEvent>{
+        /** Discard the buffer contents. Do not add the current event. */
+        DISCARD((h, e) -> h.discard()),
         /**
          * Flush the buffer to the consumer, clearing it. Do not add the current
          * event.
          */
-        FLUSH,
+        FLUSH((h, e) -> h.flush()),
         /** Add the current event to the buffer. */
-        ADD,
+        ADD((h, e) -> h.add(e)),
         /**
          * Add the current event to the buffer, then flush the buffer to the
          * consumer.
          */
-        ADD_THEN_FLUSH,
+        ADD_THEN_FLUSH((h, e) -> { h.add(e); h.flush(); }),
         /**
          * Flush the current buffer to the consumer, then add the current event
          * to the buffer.
          */
-        FLUSH_THEN_ADD,
+        FLUSH_THEN_ADD((h, e) -> { h.flush(); h.add(e); } ),
         /**
          * Discard the buffer contents, then add the current event to the
          * buffer.
          */
-        DISCARD_THEN_ADD
+        DISCARD_THEN_ADD((h, e) -> { h.discard(); h.add(e); });
+
+        private final BiConsumer<BufferingOAIEventHandler, XMLEvent> fn;
+        private BufferAction(
+                BiConsumer<BufferingOAIEventHandler, XMLEvent> fn) {
+            this.fn = fn;
+        }
+
+        @Override
+        public void accept(final BufferingOAIEventHandler t, final XMLEvent u) {
+            this.fn.accept(t,  u);
+        }
     }
 
 
@@ -65,23 +78,15 @@ public class BufferingOAIEventHandler implements OAIEventHandler {
         discard();
     }
 
+    private void add(final XMLEvent event) {
+        eventBuffer.add(event);
+    }
+
     @Override
     public void onEvent(final XMLEvent e) {
         final BufferAction action = actionFunction.apply(e);
-        if (action == BufferAction.DISCARD ||
-                action == BufferAction.DISCARD_THEN_ADD) {
-            discard();
-        } else if (action == BufferAction.FLUSH ||
-                action == BufferAction.FLUSH_THEN_ADD) {
-            flush();
-        }
-        if (action == BufferAction.ADD ||
-                action == BufferAction.FLUSH_THEN_ADD ||
-                action == BufferAction.DISCARD_THEN_ADD) {
-            eventBuffer.add(e);
-        } else if (action == BufferAction.ADD_THEN_FLUSH) {
-            eventBuffer.add(e);
-            flush();
+        if (action != null) {
+            action.accept(this, e);
         }
     }
 }
