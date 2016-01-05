631d27ef587a696d0fd16ccb297f97f0da25511a
Mike Conley <conasdf@gmail.com>
2016-01-05 12:11:52 -0500

    Add StAXImplementation.use() method.
    This tells the JDK to use the given implementation.

--- unizin-oai-harvester-parent/unizin-oai-harvester/src/test/java/org/unizin/cmp/oai/harvester/StAXImplementation.java
+++ unizin-oai-harvester-parent/unizin-oai-harvester/src/test/java/org/unizin/cmp/oai/harvester/StAXImplementation.java
@@ -1,32 +1,68 @@
 package org.unizin.cmp.oai.harvester;
 
+import java.util.Objects;
+
 import javax.xml.stream.XMLEventFactory;
 
+/** Enumeration of supported StAX implementations. */
 public enum StAXImplementation {
-    JDK,
-    WOODSTOX,
-    XERCES;
+    /** JDK built in implementation, based on Xerces. */
+    JDK("com.sun.xml.internal.stream.XMLInputFactoryImpl",
+        "com.sun.xml.internal.stream.XMLOutputFactoryImpl",
+        "com.sun.xml.internal.stream.events.XMLEventFactoryImpl"),
+    WOODSTOX("com.ctc.wstx.stax.WstxInputFactory",
+             "com.ctc.wstx.stax.WstxOutputFactory",
+             "com.ctc.wstx.stax.WstxEventFactory"),
+    XERCES(JDK.inputFactory,
+           JDK.outputFactory,
+           "org.apache.xerces.stax.XMLEventFactoryImpl");
 
+    /**
+     * Get the current implementation.
+     *
+     * @return the current StAX implementation, or {@code null} if the current
+     *         implementation is not supported or cannot be determined.
+     */
     public static StAXImplementation getImplementation() {
         return getImplementationOf(XMLEventFactory.newInstance());
     }
 
     public static StAXImplementation getImplementationOf(
             final XMLEventFactory inputFactory) {
-        final String classname = inputFactory.getClass().getName();
-        switch (classname) {
-        case "com.sun.xml.internal.stream.events.XMLEventFactoryImpl":
-            return JDK;
-        case "com.ctc.wstx.stax.WstxEventFactory":
-            return WOODSTOX;
-        case "org.apache.xerces.stax.XMLEventFactoryImpl":
-            return XERCES;
-        default:
-            return null;
+        final String className = inputFactory.getClass().getName();
+        for (final StAXImplementation impl : values()) {
+            if (Objects.equals(className, impl.eventFactory)) {
+                return impl;
+            }
         }
+        return null;
     }
 
+    /**
+     * The StAX implementation that would be used by default in this particular
+     * runtime environment.
+     */
     public static StAXImplementation DEFAULT = getImplementation();
+
+    final String inputFactory;
+    final String outputFactory;
+    final String eventFactory;
+
+    private StAXImplementation(final String inputFactory,
+            final String outputFactory, final String eventFactory) {
+        this.inputFactory = inputFactory;
+        this.outputFactory = outputFactory;
+        this.eventFactory = eventFactory;
+    }
+
+    /**
+     * Tell the JDK to use this StAXImplementation.
+     */
+    public void use() {
+        System.setProperty("javax.xml.stream.XMLInputFactory", inputFactory);
+        System.setProperty("javax.xml.stream.XMLOutputFactory", outputFactory);
+        System.setProperty("javax.xml.stream.XMLEventFactory", eventFactory);
+    }
 
     /**
      * The JDK implementation is based on Xerces. Thus, they can be expected to
