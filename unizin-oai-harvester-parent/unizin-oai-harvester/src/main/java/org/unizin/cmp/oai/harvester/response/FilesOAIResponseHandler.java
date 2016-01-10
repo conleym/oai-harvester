3646f14b521bf73a30b1e28ebb5d4e18a6b2aff8
Mike Conley <conasdf@gmail.com>
2016-01-10 12:41:59 -0500

    Allow custom file handling via indirection in FilesOAIResponseHandler.
    There's no reason to hard code a particular strategy for naming or placing
    files, so we introduce an abstraction allowing for customization. The
    previous implementation is now an instance of this abstraction.

--- unizin-oai-harvester-parent/unizin-oai-harvester/src/main/java/org/unizin/cmp/oai/harvester/response/FilesOAIResponseHandler.java
+++ unizin-oai-harvester-parent/unizin-oai-harvester/src/main/java/org/unizin/cmp/oai/harvester/response/FilesOAIResponseHandler.java
@@ -10,6 +10,7 @@ import java.util.Objects;
 import javax.xml.stream.XMLEventWriter;
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.XMLEvent;
 
 import org.unizin.cmp.oai.OAIXMLUtils;
 import org.unizin.cmp.oai.harvester.HarvestNotification;
@@ -20,34 +21,118 @@ import org.unizin.cmp.oai.harvester.exception.HarvesterException;
  * Output handler that writes each response to a separate file in a specified
  * directory.
  * <p>
- * Each file's name is the current number of responses received so far for the
- * harvest with a '.xml' extension.
+ * Each file is given by an instance of {@link EventHandlerProvider}, which
+ * provides a new {@code OAIEventHandler} for each response. Each event handler
+ * is closed once the response has been processed.
+ * </p>
+ * <p>
+ * A {@link DirectoryEventHandlerProvider handler} and
+ * {@link #FilesOAIResponseHandler(File, XMLOutputFactory)} constructor are
+ * provided for convenience.
  * </p>
  *
  */
 public final class FilesOAIResponseHandler extends AbstractOAIResponseHandler {
 
-    private final File directory;
-    private final XMLOutputFactory outputFactory;
-    private OutputStream outputStream;
-    private XMLEventWriter eventWriter;
+    /**
+     * Implementations produce an event handler given a harvest notification.
+     * <p>
+     * Instances will be called once per request/response cycle when the request
+     * is received, and must produce a <em>new</em> event handler for each
+     * response.
+     * </p>
+     */
+    @FunctionalInterface
+    public static interface EventHandlerProvider {
+        OAIEventHandler get(HarvestNotification notification)
+                throws XMLStreamException, IOException;
+    }
+
+    /**
+     * Event handler provider that creates files in a given directory.
+     * <p>
+     * Each event handler returned by this provider will write events to a file
+     * in the given directory named X.xml, where X is the current response count
+     * of the harvest. The given output factory will be used to create the event
+     * writers to which received events will be written.
+     * </p>
+     */
+    public static final class DirectoryEventHandlerProvider
+        implements EventHandlerProvider {
+
+        private final File directory;
+        private final XMLOutputFactory outputFactory;
+
+        private DirectoryEventHandlerProvider(final File directory,
+                final XMLOutputFactory outputFactory) {
+            Objects.requireNonNull(directory, "directory");
+            Objects.requireNonNull(outputFactory, "outputFactory");
+            if (! directory.isDirectory()) {
+                throw new IllegalArgumentException(String.format(
+                        "Not a directory: %s.", directory));
+            }
+            this.directory = directory;
+            this.outputFactory = outputFactory;
+        }
+
+        @Override
+        public OAIEventHandler get(final HarvestNotification notification)
+                throws XMLStreamException, IOException {
+            final long fileCount = notification.getStats()
+                    .get(HarvestStatistic.RESPONSE_COUNT);
+            final URI dest = directory.toURI().resolve(fileCount + ".xml");
+            final File destFile = new File(dest);
+            final OutputStream out = new FileOutputStream(destFile);
+            final XMLEventWriter eventWriter = outputFactory
+                    .createXMLEventWriter(out);
+            return new OAIEventHandler() {
+                @Override
+                public void onEvent(final XMLEvent e)
+                        throws XMLStreamException {
+                    eventWriter.add(e);
+                }
+
+                @Override
+                public void close() throws XMLStreamException {
+                    try (final OutputStream os = out) {
+                        eventWriter.close();
+                    } catch (final IOException e) {
+                        throw new HarvesterException(e);
+                    }
+                }
+
+            };
+        }
+    }
+
+
+    private final EventHandlerProvider provider;
     private OAIEventHandler eventHandler;
 
 
+    public FilesOAIResponseHandler(final EventHandlerProvider provider) {
+        Objects.requireNonNull(provider, "provider");
+        this.provider = provider;
+    }
+
+    /**
+     * Create an instance with a {@link DirectoryEventHandlerProvider} provider
+     * @param directory
+     */
     public FilesOAIResponseHandler(final File directory) {
         this(directory, OAIXMLUtils.newOutputFactory());
     }
 
+    /**
+     * Create an instance with a {@link DirectoryEventHandlerProvider} provider
+     * using the given directory and output factory.
+     *
+     * @param directory
+     * @param outputFactory
+     */
     public FilesOAIResponseHandler(final File directory,
             final XMLOutputFactory outputFactory) {
-        Objects.requireNonNull(directory, "directory");
-        Objects.requireNonNull(outputFactory, "outputFactory");
-        if (! directory.isDirectory()) {
-            throw new IllegalArgumentException(String.format(
-                    "Not a directory: %s.", directory));
-        }
-        this.directory = directory;
-        this.outputFactory = outputFactory;
+        this(new DirectoryEventHandlerProvider(directory, outputFactory));
     }
 
     @Override
@@ -59,14 +144,7 @@ public final class FilesOAIResponseHandler extends AbstractOAIResponseHandler {
     @Override
     public void onResponseReceived(final HarvestNotification notification) {
         try {
-            final long fileCount = notification.getStats()
-                    .get(HarvestStatistic.RESPONSE_COUNT);
-            final URI dest = directory.toURI().resolve(fileCount + ".xml");
-            final File destFile = new File(dest);
-            outputStream = new FileOutputStream(destFile);
-            eventWriter = outputFactory.createXMLEventWriter(outputStream);
-            eventHandler = new FilteringOAIEventHandler(
-                    new EventWriterOAIEventHandler(eventWriter));
+            eventHandler = provider.get(notification);
         } catch (final XMLStreamException | IOException e) {
             throw new HarvesterException(e);
         }
@@ -74,14 +152,12 @@ public final class FilesOAIResponseHandler extends AbstractOAIResponseHandler {
 
     @Override
     public void onResponseProcessed(final HarvestNotification notification) {
-        try (final OutputStream os = outputStream) {
-            if (eventWriter != null) {
-                eventWriter.close();
+        if (eventHandler != null) {
+            try {
+                eventHandler.close();
+            } catch (final XMLStreamException e) {
+                throw new HarvesterException(e);
             }
-            eventWriter = null;
-            outputStream = null;
-        } catch (final XMLStreamException | IOException e) {
-            throw new HarvesterException(e);
         }
     }
 }
